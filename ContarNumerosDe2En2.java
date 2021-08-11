ec710c62167e804dce531d366e38536a90505a60
Chanampa <lautarochampa@gmail.com>
2021-08-11 20:57:46 -0300

    Agregue esta clase donde uso un for para contar de 2 en 2

---
+++ ContarNumerosDe2En2.java
@@ -0,0 +1,15 @@
+public class ContarNumerosDe2En2 {
+
+    public static void main(String[] args) {
+//Así se puedo ir sumando número en este caso de 2 en 2, me intereso
+        for (int sumador = 0; sumador <=100; sumador++) {
+            if (sumador%2==0){
+                System.out.println(sumador);
+            }
+        }
+
+    }
+
+
+}
+
